ef44811e8897474a7432cd71fb6d46aa65a3b37a
jkee2 <jkee2@scharp.org>
2020-01-27 16:39:47 -0800

    Bring wtf-packages-report project over.

---
+++ R/01_write-installed-packages.R
@@ -0,0 +1,29 @@
+#' ---
+#' output: github_document
+#' ---
+
+## deja vu from earlier!
+
+## create a data frame of your installed packages
+## hint: installed.packages() is the function you need
+library(tidyverse)
+pkgs <- as_tibble(installed.packages())
+
+## optional: select just some of the variables, such as
+##   * Package
+##   * LibPath
+##   * Version
+##   * Priority
+##   * Built
+pkgs_selected_cols <- pkgs %>% select(Package, LibPath, Version, Priority, Built)
+
+## write this data frame to data/installed-packages.csv
+## hint: readr::write_csv() or write.table()
+## idea: try using here::here() to create the file path
+library(readr)
+library(here)
+readr::write_csv(pkgs_selected_cols, here::here("data", "installed-packages.csv"))
+
+## YES overwrite the file that is there now (or delete it first)
+## that's a old result from me (Jenny)
+## it an example of what yours should look like and where it should go
